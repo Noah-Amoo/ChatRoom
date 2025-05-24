184da7c7eef8062ac21f08e0caa4d0d0e3cdcb96
Noah Amoo <yawnoah3n@yahoo.com>
2025-05-25 00:59:20 +0100

    Initialized the templates directory with home and about html templates

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'chatroom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
